651e28f187d5abcc3884df299bbeff888f52a8b6
mahahahad <mahad.abd2003@gmail.com>
2023-05-04 18:51:58 +0400

    Add initial files

---
+++ index.ts
@@ -0,0 +1,26 @@
+const canvasEl = document.querySelector("#canvas") as HTMLCanvasElement;
+
+if (canvasEl.getContext) {
+  let ctx = canvasEl.getContext("2d") as CanvasRenderingContext2D;
+
+  // Take up full screen size
+  canvasEl.height = window.innerHeight;
+  canvasEl.width = window.innerWidth;
+
+  // Initialize wave starting point and line details
+  let xVal = 0;
+  ctx.beginPath();
+  ctx.moveTo(0, innerHeight / 2);
+  ctx.strokeStyle = "white";
+
+  function draw() {
+    let xCord = Math.PI * xVal * 16;
+    let yCord = innerHeight / 2 + Math.sin(Math.PI * xVal) * 100;
+    ctx.lineTo(xCord, yCord);
+    xVal += 0.05;
+    ctx.stroke();
+    requestAnimationFrame(draw);
+  }
+
+  draw();
+}
